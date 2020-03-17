37c16310dbe3102d70043c6feb91c2da0c01c981
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-17 13:53:38 -0500

    Create String_Concatenation.js

---
+++ String_Concatenation.js
@@ -0,0 +1,13 @@
+/*
+console.log('front ' + 'space'); 
+// Prints 'front space'
+console.log('back' + ' space'); 
+// Prints 'back space'
+console.log('no' + 'space'); 
+// Prints 'nospace'
+console.log('middle' + ' ' + 'space'); 
+// Prints 'middle space'
+*/
+
+console.log('Hello' + 'World')
+console.log('Hello' +' '+ 'World')
