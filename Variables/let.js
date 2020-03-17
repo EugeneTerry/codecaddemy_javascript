c0682ed55beaf21d4d9476ce4238a21bc31aa5d1
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-17 16:33:39 -0500

    Update and rename let.js to let_vs_var_vs_const.js
    Added the 'var" and 'const' statements

--- Variables/let.js
+++
@@ -1,3 +0,0 @@
-let changeMe = true //allows the variable to be changed lateron without typing "let"
-changeMe = false //this changed the variable to false
-console.log(changeMe) 
---
+++ Variables/let_vs_var_vs_const.js
@@ -0,0 +1,4 @@
+let changeMe = true //allows the variable to be changed lateron without typing "let" over again
+// var and const cannot be changed 
+changeMe = false //this changed the variable to false
+console.log(changeMe) 
