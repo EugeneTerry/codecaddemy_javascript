95fd4a272f02cb118584a83e1f0576baa8f9afe1
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-17 16:42:32 -0500

    Create using_variables_to_do_math.js

---
+++ Math/using_variables_to_do_math.js
@@ -0,0 +1,18 @@
+// variables assigned values
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// mathematical assignments to the variables
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+// These console.log() statements below check the values of the variables.
+ 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
