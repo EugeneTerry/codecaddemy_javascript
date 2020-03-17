6625811a114dad4133b45e610974e8f0a19c8d3b
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-17 14:37:10 -0500

    Create Math_objects_built_into_JavaScript.js

---
+++ Math/Math_objects_built_into_JavaScript.js
@@ -0,0 +1,4 @@
+console.log(Math.random() * 100) // Math.random() generates numbers between 0 and 1
+console.log(Math.floor(Math.random() * 100)) //Math.floor() maks the random number a whole number.
+console.log(Math.ceil((Math.random() * 43.8) )) // Math.ceil() returns the smallest integer greater than or equal to a decimal number.
+console.log(Number.isInteger(2017)) //Number.isInteger() checks if a number is an integer.
