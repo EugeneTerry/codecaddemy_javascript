fd78e0471038bb83a23aafd2fd8557348a69662e
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-18 14:35:50 -0500

    Create intro_into_if_else.js

---
+++ Conditional_statements/intro_into_if_else.js
@@ -0,0 +1,9 @@
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!'); //this will display if the the condition is true
+} else {
+    console.log('Time to wait for a sale.') //this will display if the the condition is false
+}
