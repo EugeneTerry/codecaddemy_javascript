377d318284afa9badddc0dc98228ddff5b00ee9d
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-18 18:52:51 -0500

    Create else_if_statements.js

---
+++ Conditional_statements/else_if_statements.js
@@ -0,0 +1,15 @@
+// adding else if statements when you want to do more than two outcomes for conditions
+
+let season = 'summer';
+// this will display a statement depending on the season assigned to the variable
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.')
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!')
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!')
+} else {
+  console.log('Invalid season.');
+}
