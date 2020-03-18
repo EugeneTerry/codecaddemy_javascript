9b0de31656ba416cc364604d77eaa489b94681bc
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-18 18:40:07 -0500

    Create Changing_if_else_to_Ternary_operators.js

---
+++ Conditional_statements/Changing_if_else_to_Ternary_operators.js
@@ -0,0 +1,13 @@
+/* Changing this:
+let favoritePhrase = 'Love That!';
+
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+}
+to this:
+*/
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
