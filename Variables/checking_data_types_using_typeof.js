b92acde3eb813df741e0786d9289c63c7cde1243
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-17 21:27:46 -0500

    Create checking_data_types_using_typeof.js

---
+++ Variables/checking_data_types_using_typeof.js
@@ -0,0 +1,7 @@
+// check the data type of a variable’s value, you can use the typeof operator
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+// this diplays the word "string" because the variable has letters in it
+newVariable = 1 //changed the string variable to a number
+console.log(typeof newVariable);
+// this diplays the word "number" because the variable is a number now
