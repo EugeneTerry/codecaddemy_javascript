ea34fec958ff103196cf85ad92d33bc445eb799c
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-17 21:20:42 -0500

    Create String_Interpolation_using_${}.js

---
+++ String_Interpolation_using_${}.js
@@ -0,0 +1,5 @@
+// here we use the `${}` to insert a variable into a sentence to display as its value.
+// you have to use the " ` " symbol next to the "1" on the keyboard.
+let myName = 'Eugene'
+let myCity = 'Antioch'
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`) // this displays "My name is Eugene. My favorite city is Antioch."
