477c950a88296a028fcbc5b5aa59961b9b78f700
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-18 14:49:04 -0500

    Create Comparison_symbols.js

---
+++ Math/Comparison_symbols.js
@@ -0,0 +1,13 @@
+let hungerLevel = 7;
+if (hungerLevel <= 7) {
+    console.log('Time to eat!'); // this will diplay if the statement is true
+} else {
+  console.log('We can eat later!'); // this will diplay if the statement is false
+}
+/* Less than: <
+Greater than: >
+Less than or equal to: <=
+Greater than or equal to: >=
+Is equal to: ===
+Is NOT equal to: !==
+*/
