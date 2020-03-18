243385581fe2d00b0054197abc3db582733a063d
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-17 20:59:40 -0500

    Create Increment_and_Decrement_Operator.js

---
+++ Math/Increment_and_Decrement_Operator.js
@@ -0,0 +1,7 @@
+// using the ++ to go up by 1 and the -- to go down by 1
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar ++;
+console.log(gainedDollar) //should display 4
+lostDollar --;
+console.log(lostDollar) //should display 49
