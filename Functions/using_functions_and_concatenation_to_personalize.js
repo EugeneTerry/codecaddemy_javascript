e815d2d4c311f0ae02d63f8b378e8115a502d5a7
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-19 13:34:28 -0500

    Create using_functions_and_concatenation_to_personalize.js

---
+++ Functions/using_functions_and_concatenation_to_personalize.js
@@ -0,0 +1,6 @@
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+sayThanks('Cole');
+// this allows Cole to be used in the thank you statement on the recipt.
+//so it will diplay "Thank you for your purchase Cole! We appreciate your business."
