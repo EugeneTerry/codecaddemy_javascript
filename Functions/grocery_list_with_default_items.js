0812ad097e16c9e32ab33b8eefe3dfda2aa9585c
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-19 13:43:05 -0500

    Create grocery_list_with_default_items.js

---
+++ Functions/grocery_list_with_default_items.js
@@ -0,0 +1,11 @@
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+makeShoppingList()
+/*this displays
+Remember to buy milk
+Remember to buy bread
+Remember to buy eggs
+*/
