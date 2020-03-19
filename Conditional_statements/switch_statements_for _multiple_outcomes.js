405a6b338ae368312ebe4dd2119c0afbbf3cabd5
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-18 20:01:48 -0500

    Create switch_statements_for _multiple_outcomes.js

---
+++ Conditional_statements/switch_statements_for _multiple_outcomes.js
@@ -0,0 +1,16 @@
+// this is to replace else if statements Trin says it will be a situational thing to use either or
+let athleteFinalPosition = 'second place';
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log ('You get the gold medal!');
+    break;
+case 'second place':
+    console.log ('You get the silver medal!');
+    break;
+case 'third place':
+    console.log ('You get the bronze medal!');
+    break;
+  default:
+  console.log('No medal awarded.');
+    break;
+}
