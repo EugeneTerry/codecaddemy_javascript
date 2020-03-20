28bf9db42890f3b34c7c3efc510b5a639bbc91af
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-20 12:59:32 -0500

    Create Area_function.js

---
+++ Math/Area_function.js
@@ -0,0 +1,6 @@
+//this function will find the area of a quadralateral using lenth times width.
+function monitorCount(lenth, width) {
+ return lenth * width
+}
+const areaOfShape = monitorCount(5 , 4) // enter values here
+console.log(areaOfShape)
