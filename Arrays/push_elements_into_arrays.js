b5ed45bfeedaa05318763be007fe4b864d450394
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-25 21:53:44 -0500

    Create push_elements_into_arrays.js

---
+++ Arrays/push_elements_into_arrays.js
@@ -0,0 +1,10 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash']; //original array
+chores.push('sweep floors', 'feed dogs');
+console.log(chores); 
+/* prints 
+"[ 'wash dishes',
+  'do laundry',
+  'take out trash',
+  'sweep floors',
+  'feed dogs' ]
+*/
