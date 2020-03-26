6f82c113259a8e3a657a3c2228896be5bb31aefa
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-25 22:57:56 -0500

    Create access_array_data_within_nested_arrays.js

---
+++ Arrays/access_array_data_within_nested_arrays.js
@@ -0,0 +1,3 @@
+const numberClusters = [[1, 2], [3, 4], [5, 6]];
+const target = numberClusters[2][1]; //calls "item01" which is '6' within the parent "item02" which is '[5, 6]
+console.log(target); //prints "6"
