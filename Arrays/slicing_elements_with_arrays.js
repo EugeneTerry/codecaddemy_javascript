1a6fbd170c985f801a921682cc2c162fbfd5a467
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-25 22:15:18 -0500

    Create slicing_elements_with_arrays.js

---
+++ Arrays/slicing_elements_with_arrays.js
@@ -0,0 +1,3 @@
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+console.log(groceryList.slice(1, 4)); //this takes all of the elements out except the "item01" and the "item03" it does not keep the "item04"
+// prints [ 'bananas', 'coffee beans', 'brown rice' ]
