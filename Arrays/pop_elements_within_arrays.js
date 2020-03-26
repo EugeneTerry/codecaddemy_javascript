351eff761f089e579c1644e3a94d2f66bed3c5f9
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-25 21:57:07 -0500

    Create pop_elements_within_arrays.js

---
+++ Arrays/pop_elements_within_arrays.js
@@ -0,0 +1,3 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor']; //original array
+chores.pop();//takes out the last element
+console.log(chores); //prints"[ 'wash dishes', 'do laundry', 'take out trash', 'cook dinner' ]"
