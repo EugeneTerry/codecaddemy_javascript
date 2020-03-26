490d4b3f241e7a87fadb0e73bf3368f7d2dc4a42
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-25 23:00:54 -0500

    Update indexing_elements_within_an_array.js
    added statements to overlook two lines of codes

--- Arrays/indexing_elements_within_an_array.js
+++ Arrays/indexing_elements_within_an_array.js
@@ -1,5 +1,5 @@
 const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
-groceryList.shift();
-groceryList.unshift('popcorn');
+groceryList.shift(); // never mind this code
+groceryList.unshift('popcorn'); // never mind this code
 const pastaIndex = groceryList.indexOf('pasta'); //index identifies the item# of an element
 console.log(pastaIndex); //this printed "4" because "pasta" is "item04" in the array
