19882e734653d6c8b8ea807c859a32e90a8b5187
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-26 13:52:25 -0500

    Create looping_through_ararys.js

---
+++ Loops/looping_through_ararys.js
@@ -0,0 +1,8 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+for (let i = 0; i < vacationSpots.length; i++) { // this goes through the items in asending order
+  console.log('I would love to visit ' + vacationSpots[i]);
+/* prints
+"I would love to visit Bali
+I would love to visit Paris
+I would love to visit Tulum"
+*/
