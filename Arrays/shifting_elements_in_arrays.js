405764f69d5c705b8c712008092c4512fd0544cc
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-25 22:01:26 -0500

    Create shifting_elements_in_arrays.js

---
+++ Arrays/shifting_elements_in_arrays.js
@@ -0,0 +1,11 @@
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.shift(); //takes the first element out
+console.log(groceryList);
+/* prints
+"[ 'bananas',
+  'coffee beans',
+  'brown rice',
+  'pasta',
+  'coconut oil',
+  'plantains' ]"
+  */
