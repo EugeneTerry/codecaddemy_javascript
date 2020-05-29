f5b58af89c13d353d9e923de95aca64b31717765
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-05-29 13:38:15 -0500

    Update map_through_arrays.js

--- Loops/map_through_arrays.js
+++ Loops/map_through_arrays.js
@@ -9,3 +9,27 @@ console.log(squareNumbers);
 /* this will display
 [ 1, 4, 9, 16, 25 ]
 */
+
+/* here is my attempt*/
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+const secretMessage = animals.map(animal => {
+  return animal[0]; /* this took the first letter from each word in the array */
+})
+
+console.log(secretMessage.join(''));
+/* this printed out
+HelloWorld
+*/
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(number => {
+  return number / 100; /* this divided each number in the array by 100 */
+  });
+  console.log(smallNumbers);
+/* this diplayed 
+[ 1, 2, 3, 4, 5 ]
+*/
