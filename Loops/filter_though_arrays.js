ebc3b4ec71d479f070c038e12aad61c2710baee8
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-05-29 14:02:32 -0500

    Update filter_though_arrays.js

--- Loops/filter_though_arrays.js
+++ Loops/filter_though_arrays.js
@@ -8,3 +8,10 @@ console.log(onlyNumbers);
 /* this will display
 [ 5, 3.14, 100 ]
 */
+// this is my attampt
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+const smallNumbers = randomNumbers.filter(number => {
+  return number < 250; //this filter out every number that was bigger than 250
+})
+ console.log(smallNumbers); // this displayed [200, 3.14, 7, 13]
