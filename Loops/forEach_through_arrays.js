a21230c6b6e2b5ae6c028c5e79e3eab56ed9aa90
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-05-29 12:43:20 -0500

    Create forEach_through_arrays.js

---
+++ Loops/forEach_through_arrays.js
@@ -0,0 +1,11 @@
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+/* this will print out
+Picasso is one of my favorite artists.
+Kahlo is one of my favorite artists.
+Matisse is one of my favorite artists.
+Utamaro is one of my favorite artists.
+/*
