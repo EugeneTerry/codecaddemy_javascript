824548005be05265657a9ba04bb8537b623b14a3
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-05-29 13:11:51 -0500

    Update forEach_through_arrays.js

--- Loops/forEach_through_arrays.js
+++ Loops/forEach_through_arrays.js
@@ -9,3 +9,18 @@ Kahlo is one of my favorite artists.
 Matisse is one of my favorite artists.
 Utamaro is one of my favorite artists.
 */
+
+
+// this is my attempt 
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(fruit => {
+  console.log('I want to eat a '+ fruit + '.')
+  });
+/* this will print out
+I want to eat a mango.
+I want to eat a papaya.
+I want to eat a pineapple.
+I want to eat a apple.
+*/
