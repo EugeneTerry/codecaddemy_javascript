4a819c9c8ffdbf1b47f91a2efe4b0e4c94adb796
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-05-29 12:50:17 -0500

    Create map_through_arrays.js

---
+++ Loops/map_through_arrays.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+/* this will display
+[ 1, 4, 9, 16, 25 ]
+/*
