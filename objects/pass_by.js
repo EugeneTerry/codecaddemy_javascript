25701a6876259a4dd05b172ba59ff421024b7cc9
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-06-03 17:31:34 -0500

    Create pass_by.js

---
+++ objects/pass_by.js
@@ -0,0 +1,19 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil' //this changes tje fuel type to avocado oil
+};
+let remotelyDisable = obj => {
+  obj.disabled = true this adds to the spaceship a disable item with the property of "true"
+  }
+greenEnergy(spaceship)
+remotelyDisable(spaceship)
+console.log(spaceship)
+/* so the new object should pront this out
+{ 'Fuel Type': 'avocado oil',
+  homePlanet: 'Earth',
+  disabled: true }
+  */
