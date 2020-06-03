bf34204211e6031ea037f71c1be1db092d6b43a3
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-06-03 12:04:21 -0500

    Create accessing_properties.js

---
+++ objects/accessing_properties.js
@@ -0,0 +1,11 @@
+// created a spaceship with a lot of discriptions by using objects
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+// this next two lines created variables with the values of certain properties of the spaceship
+let crewCount = spaceship.numCrew;
+let planetArray = spaceship.flightPath;
