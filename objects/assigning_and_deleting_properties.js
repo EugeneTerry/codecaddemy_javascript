b7d7f1ace8253aa0ea7fd9a9a7a8abc4f3efad20
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-06-03 12:49:30 -0500

    Create assigning_and_deleting_properties.js

---
+++ objects/assigning_and_deleting_properties.js
@@ -0,0 +1,10 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold'; //changing the color property
+spaceship.numEngines = 8; //adding a property to the spaceship
+delete spaceship['Secret Mission']; //deleting a property from the spaceship
