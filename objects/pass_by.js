9aba4a41aa14fee756a62985855fdc7f4d92a99e
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-06-03 17:39:06 -0500

    Update pass_by.js

--- objects/pass_by.js
+++ objects/pass_by.js
@@ -1,14 +1,16 @@
+  
 let spaceship = {
   'Fuel Type' : 'Turbo Fuel',
   homePlanet : 'Earth'
 };
 
+// Write your code below
 let greenEnergy = obj => {
   obj['Fuel Type'] = 'avocado oil' //this changes tje fuel type to avocado oil
 };
 let remotelyDisable = obj => {
-  obj.disabled = true //this adds to the spaceship a disable item with the property of "true"
-  }
+  obj.disabled = true 
+  } //this adds to the spaceship a disable item with the property of "true"
 greenEnergy(spaceship)
 remotelyDisable(spaceship)
 console.log(spaceship)
