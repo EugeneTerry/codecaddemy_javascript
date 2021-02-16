1de02377df7a31eca1e0f2ea7dae228cf8862027
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-02-15 21:43:00 -0600

    Update intro_into_if_else.js
    add new statement

--- Conditional_statements/intro_into_if_else.js
+++ Conditional_statements/intro_into_if_else.js
@@ -7,3 +7,16 @@ if(sale) {
 } else {
     console.log('Time to wait for a sale.') //this will display if the the condition is false
 }
+/* Less than: <
+Greater than: >
+Less than or equal to: <=
+Greater than or equal to: >=
+Is equal to: ===
+Is NOT equal to: !==
+*/
+let newStudent = 15;
+if(newStudent >= 13) {
+  console.log(`This student needs to be in the next grade. They are ${newStudent} years old`);
+} else {
+  console.log(`This student is in the correct grade`);
+}
