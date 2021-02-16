21a072856f372940fc4f5a9ecbcdcd65519d457d
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-02-15 21:57:47 -0600

    Update Logical_operators.js
    add practice code

--- Conditional_statements/Logical_operators.js
+++ Conditional_statements/Logical_operators.js
@@ -9,3 +9,10 @@ the and operator (&&)
 the or operator (||)
 the not operator, otherwise known as the bang operator (!)
 */
+let fastFood = 'hungry'
+let thirstLevel = 15
+if( fastFood === 'hungry' && thirstLevel >=9) {
+  console.log(`You need to order the whole combo`)
+} else {
+  console.log(`You can just order the sandwich only`)
+}
