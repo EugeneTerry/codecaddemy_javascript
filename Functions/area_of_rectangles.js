3a4e3c94cb7444164798ca655d80af251cf23f5a
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-02-17 12:21:08 -0600

    Update area_of_rectangles.js
    add triangle area

--- Functions/area_of_rectangles.js
+++ Functions/area_of_rectangles.js
@@ -6,3 +6,15 @@ function rectangleDimensions(lenth, width) {
 }
 const areaOfShape = rectangleDimensions(15 , 4)
 console.log(areaOfShape)
+
+// this next one givess the area of a triangle
+function mesurementsTri(base, height) {
+  return base * height;
+}
+function areaCal1(base, height) {
+   if (base < 0 || height < 0) {
+   return `Either ${base} or ${height} is not a positive interger. Input another number please.`;
+   }
+  return mesurementsTri(base, height) * 0.5;
+};
+console.log(areaCal1(-8, 9));
