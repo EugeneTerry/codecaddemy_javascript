dd102a2b596ee9258e37049a8477901a011e0f83
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-02-21 17:55:19 -0600

    Update Do_While_loop.js
    adjusted comments

--- Loops/Do_While_loop.js
+++ Loops/Do_While_loop.js
@@ -5,7 +5,7 @@ let i = 1;
 
 do {
   cupsAdded = cupsAdded + i; //this means to add one cup of sugar no matter what the recipe says
-  i++;  // this will go up one cup every time it goes throught the loop like 2+3+4+5 nad will stop adding cups at 5
-} while (i < cupsOfSugarNeeded); 
+  i++;  // this will go up one cup every time it goes throught the loop like 2+3+4+5 nad will stop adding cups at 6
+} while (i <= cupsOfSugarNeeded); 
 console.log(cupsAdded)
-// since the cupsOfSugarNeeded = 6 it will print "15"
+// since the cupsOfSugarNeeded = 6 it will print "21"
