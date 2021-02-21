97d9fc664e29fac300126cc876a41916881f8e4e
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-02-20 20:13:28 -0600

    Update access_array_data_within_nested_arrays.js
    add new practice

--- Arrays/access_array_data_within_nested_arrays.js
+++ Arrays/access_array_data_within_nested_arrays.js
@@ -1,3 +1,9 @@
 const numberClusters = [[1, 2], [3, 4], [5, 6]];
 const target = numberClusters[2][1]; //calls "item01" which is '6' within the parent "item02" which is '[5, 6]
 console.log(target); //prints "6"
+
+const nestArr = [['solo'], ['duet01', 'duet02'], ['trio01','trio02', 'trio03']];
+console.log(nestArr[2]); // print outs  ["trio01", "trio02", "trio03"]
+const duo = nestArr[1]; // assign a nested item to the variable called 'duo'
+console.log(duo);// prints out  ["duet01", "duet02"]
+console.log(nestArr[2][1]); // prints out "trio02"
