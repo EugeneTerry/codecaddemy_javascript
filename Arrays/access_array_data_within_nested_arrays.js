7105fd8871ab279bf8be31fe98c39e120ab9d82a
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-02-20 21:23:04 -0600

    Update access_array_data_within_nested_arrays.js
    add comments

--- Arrays/access_array_data_within_nested_arrays.js
+++ Arrays/access_array_data_within_nested_arrays.js
@@ -7,5 +7,5 @@ console.log(nestArr[2]); // print outs  ["trio01", "trio02", "trio03"]
 const duo = nestArr[1]; // assign an item to the variable called 'duo'
 console.log(duo);// prints out  ["duet01", "duet02"]
 console.log(nestArr[2][1]); // prints out "trio02"
-nestArr.splice([0],[2]);
-console.log(nestArr);
+nestArr.splice([0],[2]); //this deletes "item 0" along with one other next item "item 1" so 2 items in all
+console.log(nestArr); //prints out "[ [ 'trio01', 'trio02', 'trio03' ] ]"
