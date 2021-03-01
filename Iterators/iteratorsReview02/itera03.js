6d772770b673e4ace0147cf1889b868ec63cc8e3
Eugene Terry <eugene.terry73@gmail.com>
2021-02-28 21:31:52 -0600

    added an .every method

--- Iterators/iteratorsReview02/itera03.js
+++ Iterators/iteratorsReview02/itera03.js
@@ -4,3 +4,7 @@ const sumClasses = classSize.reduce((firstNum, currentNum) => { // this line loo
     return firstNum + currentNum; // this just does math to the two values it creates 
 });
 console.log(`The average of ${classSize.join(', ')} is ${sumClasses/classSize.length}.`); // this just takes the return values and divides them by the number of items using ".lenght"
+
+console.log(family.every((kid) => { // this is seeing if everything is the same in the array
+    return kid.length > 5; // this says what the same is
+}));
