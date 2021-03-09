7d13897f6542c4412e0076470a3c9faa1cf892a3
Eugene Terry <eugene.terry73@gmail.com>
2021-03-08 22:02:42 -0600

    update

---
+++ objects/object04.js
@@ -0,0 +1,31 @@
+console.log(`//////////////Looping Through Objects///////////////`)
+let redBark = {
+    occupant:[{
+        name: 'Gabrielle'
+    }],
+    house: {
+        yearBuilt: 2004,
+        color: 'Brick',
+        fence: false,
+    },
+    owners: {
+        wife: {
+            name: 'Teresa',
+            degree: 'Ed.D',
+            saying() { console.log(`I'm Big Sexy!`)},
+            'Favorite Food': ['rice', 'beans', 'chicken', 'pork chops']
+        },
+        husband: {
+            name: 'Eugene',
+            degree: 'M.Ed',
+            saying() {console.log(`Everything has its place`)},
+            'Favorite Food':['pizza', 'spaghetti', 'catfish']
+        }
+        }
+    };
+for (let houseOwer in redBark.owners) { // this for...in loop creates a temp variable that gose through each property 
+    console.log(`${houseOwer}: ${redBark.owners[houseOwer].name}`); 
+}
+
+for (let houseOwer in redBark.owners) { // this for...in loop creates a temp variable that gose through each property 
+    console.log(`${redBark.owners[houseOwer].name}: ${redBark.owners[houseOwer].degree}`); }
