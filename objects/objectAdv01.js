cdde32da77ee9a4064780960eb09a9ba54331443
Eugene Terry <eugene.terry73@gmail.com>
2021-03-11 21:11:30 -0600

    New Add

--- objects/objectAdv01.js
+++ objects/objectAdv01.js
@@ -22,3 +22,28 @@ const dog2 = {
     }
 }
 dog2.collarType();
+
+console.log(`//////////////Getters///////////////`)
+
+const terryFam = {
+    _firstName: 'Dess',
+    _lastName: 'Terry',
+    /*get fullName () {
+        if (this._firstName && this._lastName){
+        return `${this._firstName} ${this._lastName}`;
+        } else {
+            return 'Missing first or last name.';
+        }
+    }*/
+    get lastNameCheck () { // will get a property from an object and return something as a result of some perameters using an if statement
+        if (typeof this._lastName === 'string'){
+            return `${this._firstName} ${this._lastName}`;
+        } else {
+            return `${this._firstName} is missing a last name because it is a ${typeof terryFam._lastName}`;
+        }
+    }
+}
+    console.log(terryFam.lastNameCheck);// prints the full name depending on the propery type.
+
+    console.log(`//////////////Setters///////////////`)
+
