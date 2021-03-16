4cac8571dd522c77e0582bf3ef0f2f04f796cee1
Eugene Terry <eugene.terry73@gmail.com>
2021-03-15 23:07:07 -0500

    last update for code Academy

--- objects/objectAdv02.js
+++ objects/objectAdv02.js
@@ -22,5 +22,6 @@ const kid03 = terryFactory('Gabrielle', 16, 'art', 'thats sus');
 const kid04 = terryFactory('Dess', 11, 'crafts', 'i dont know');
 console.log(kid01.name);
 console.log(kid02.name);
-console.log(kid03.name);
-console.log(kid04.name);
+const newCat = Object.assign({shoeSize: 'n/a'}, terryFactory);
+console.log(newCat);
+
